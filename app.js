8c5e0800dbd23bf5d26004982c4a6c10bd051886
Kakamotobi <79886384+Kakamotobi@users.noreply.github.com>
2021-05-20 11:42:56 +0900

    Update app.js
    Email validation.

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ const emailSubBtn = document.querySelector(".emailSubBtn");
 const errorIcon = document.querySelector(".errorIcon");
 const errorMsg = document.querySelector(".errorMsg");
 
-emailSubBtn.addEventListener("click", function () {
-    if (emailSubInput.validity.typeMismatch) {
+emailSubForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+
+    const emailTest = emailSubInput.value;
+
+    if (!isValidEmail(emailTest)) {
         emailSubInput.classList.add("errorBorder");
         errorIcon.style.display = "inline";
         errorMsg.style.display = "block";
+        console.log(`${emailTest} is invalid`);
     } else {
-        emailSubForm.addEventListener("submit", function (evt) {
-            evt.preventDefault();
-            console.log(emailSubInput.value);
-            emailSubInput.classList.remove("errorBorder");
-            errorIcon.style.display = "none";
-            errorMsg.style.display = "none";
-            emailSubInput.value = "";
-        });
+        emailSubInput.classList.remove("errorBorder");
+        errorIcon.style.display = "none";
+        errorMsg.style.display = "none";
+        emailSubInput.value = "";
+        console.log(`${emailTest} is valid`);
     }
 });
+
+// Email validation
+function isValidEmail(emailTest) {
+    return /^([A-Za-z0-9_-]+\.)*[A-Za-z0-9_-]+\@([A-Za-z0-9_-]+\.)+[A-Za-z]{2,4}$/.test(
+        emailTest
+    );
+}
